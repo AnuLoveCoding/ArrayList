c9977d5eab2302495bd96c690a511c608473bd6e
anuLoveCoading <anusrivas07@gmail.com>
2022-11-14 21:45:27 +0530

    git commit -m "ArrayList"

---
+++ src/problem1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+//import  java.util.*;
+
+public class problem1 {
+    public static void main(String[] args) {
+        ArrayList<Integer>list= new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(60);
+        System.out.println(list);
+        System.out.println(list.get(2));
+
+        list.set(2,45);
+        System.out.println(list);
+
+        list.remove(1);
+        System.out.println(list);
+        list.add(2,35);
+
+    }
+}
