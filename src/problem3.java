70bf2763e196b56504bd00176e69e1655218627e
anuLoveCoading <anusrivas07@gmail.com>
2022-11-16 21:06:44 +0530

    git commit -m "ArrayList"

--- src/problem3.java
+++ src/problem3.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class problem3 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int s = 'a'+'b';
+        System.out.println(s);
+
+
+    }
 }
