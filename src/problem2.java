52cd6b99c8ee68bd73f12f25e80bd5dfe646ab2f
anuLoveCoading <anusrivas07@gmail.com>
2022-11-18 19:10:34 +0530

    git commit -m "ArrayList"

--- src/problem2.java
+++ src/problem2.java
@@ -1,7 +1,31 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class problem2 {
+//    (Q.1)
+    public static int[] solve(int A,int B,int [][] C){
+        int [] sum_arr = new int[B];
+
+        for(int j=0;j<B;j++){
+            for(int i=0;i<A;i++){
+                sum_arr[j] += C[i][j];
+            }
+        }
+
+        return sum_arr;
+    }
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+      /* (Q.1) Given a 2D integer array C[][] of A rows and B columns. Return an integer array of size B that
+       represents the sums of the columns of the 2D array C.
+       */
+        int A = scn.nextInt();
+        int B = scn.nextInt();
+
+        int [][] C = new int[A][B];
+
+        System.out.println(solve(A,B,C));
+
 
     }
 }
